4c6c9ec775e30f77fd92c343fe01f0bc064d5083
daikonnbatake <taiseie.nmz.dsk@gmail.com>
2022-05-04 07:10:20 +0900

    user登録やつ

--- api/routers_v1/user.py
+++ api/routers_v1/user.py
@@ -1,15 +1,38 @@
+import os, sys; sys.path.append(os.pardir)
+
+from utilities.db_access import DBAccess as db
+from my_typing.request_body import PostUserAdd
+from my_typing.response import ResponseErrorCode
 from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get('/v1/user/{user_id}/settings', tags=['user'])
+# ユーザーを新規登録
+@router.post('/v1/users/add', tags=['users'], response_model=ResponseErrorCode)
+async def postUserAdd(user_id: PostUserAdd):
+    ret = ResponseErrorCode(error_code=0, message='success')
+    res = db.insert('users', ['id'], [user_id])
+    if res == 'error':
+        ret.error_code = 1
+        ret.message = 'userid "%s" is exist.' % user_id
+    return ret
+
+# 自習ランクを取得
+@router.get('/v1/users/{user_id}/rank', tags=['users'])
+async def getUserRank(user_id):
+    return {'user_rank': []}
+
+# 自習レベルを取得
+@router.get('/v1/users/{user_id}/level', tags=['users'])
+async def getUserRank(user_id):
+    return {'user_level': []}
+
+# ユーザーの設定情報を取得
+@router.get('/v1/users/{user_id}/settings', tags=['users'])
 async def getUserSettings(user_id):
     return {'user_settings': []}
 
-@router.post('/v1/user/add')
-async def postUserAdd():
-    return {'err': 0}
-
-@router.put('/v1/user/{user_id}/settings', tags=['user'])
+# ユーザーの設定情報を更新
+@router.put('/v1/users/{user_id}/settings', tags=['users'])
 async def putUserSettings(user_id):
     return {'err': 0}
